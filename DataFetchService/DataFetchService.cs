d13c0038ce6c2c6033881ce1a6cec4de861ab9f5
Prajwol Sangat <psangat@hotmail.com>
2017-04-28 12:35:59 +1000

    Working on Async Task

---
+++ DataFetchService/DataFetchService.cs
@@ -0,0 +1,121 @@
+﻿using Agilent.AtomicSpectroscopy.ICP.Protocol.Scp;
+using Agilent.Nexus.Protocol.Scp;
+using Agilent.Nexus.ScpClient.Connection;
+using Agilent.Nexus.ScpClient.PublicInterfaces;
+using Agilent.Nexus.ScpClient.ScpHandler;
+using Agilent.Nexus.ScpClient.SLIP;
+using Agilent.Nexus.Xdr;
+using DataFetchService;
+using RFControlAsyncApp;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataFetchService
+{
+    class DataFetchService
+    {
+        private ProtocolSender _packetSender;
+        private ProtocolReceiver _packetReceiver;
+        private ConnectionTcp _instrumentConnection;
+        private AuroraDiagnosticPort _diagnosticPort;
+        private const int _serverPort = 8765;
+
+        
+
+        public void connect_To_Instrument(string ipAddress)
+        {
+            _instrumentConnection = new ConnectionTcp(ipAddress, _serverPort);
+            _packetSender = new ProtocolSender(_instrumentConnection);
+            _packetReceiver = new ProtocolReceiver(_instrumentConnection);
+            ProtocolAuroraResponseMapper.InitialiseAuroraResponseMap(_packetReceiver);
+            _diagnosticPort = new AuroraDiagnosticPort(_packetSender, _packetReceiver);
+
+            try
+            {
+                Console.WriteLine("Connecting to {0}", ipAddress);
+                if (_instrumentConnection.State == ConnectionState.Connected)
+                {
+                    _diagnosticPort.Close();
+                    _instrumentConnection.Disconnect();
+                }
+
+                if (_instrumentConnection.Connect(ipAddress, _serverPort))
+                {
+                    _diagnosticPort.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in instrument connection");
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        public int SubsystemID
+        {
+            get { return 0x60; }
+        }
+
+      
+         
+      
+        public async Task<AsyncSsrfStatus> getSSRFMeasurements()
+        {
+            AsyncSsrfStatus _ssrfStatus = new AsyncSsrfStatus();
+            await new Task(()=>
+            {
+                try
+                {
+                    if (_instrumentConnection.State == ConnectionState.Connected)
+                    {
+                        DirectSlipServer _server = new DirectSlipServer(_diagnosticPort);
+                        AsyncAlarmStatus _alarmStatus = new AsyncAlarmStatus();
+
+                        _ssrfStatus.Configure(_server, SubsystemID);
+
+                        while (_ssrfStatus.StatusUpdated <= 0)
+                        {
+                            // wait for status update
+                        }
+                        // _alarmStatus.Configure(_server, SubsystemID);
+
+                        if (_ssrfStatus.StatusUpdated > 0)
+                        {
+                            Console.WriteLine("Dac0FeedbackVolts: {0}", _ssrfStatus.Dac0FeedBackVolts);
+                            Console.WriteLine("HVPSVolts: {0}", _ssrfStatus.HVPSVolts);
+                            Console.WriteLine("HVPSAmps: {0}", _ssrfStatus.HVPSAmps);
+                            Console.WriteLine("HVPSSymmetryVolts: {0}", _ssrfStatus.HVPSSymmetryVolts);
+                            Console.WriteLine("Temp1Celsius: {0}", _ssrfStatus.Temp1Celsius);
+                            Console.WriteLine("Temp2Celsius: {0}", _ssrfStatus.Temp2Celsius);
+                            Console.WriteLine("Temp3Celsius: {0}", _ssrfStatus.Temp3Celsius);
+                            Console.WriteLine("DogBoneCelsius: {0}", _ssrfStatus.DogBoneCelsius);
+                            Console.WriteLine("WorkCoilAmps: {0}", _ssrfStatus.WorkCoilAmps);
+                            Console.WriteLine("WorkCoilHertz: {0}", _ssrfStatus.WorkCoilHertz);
+                            Console.WriteLine("OscillatorHertz: {0}", _ssrfStatus.OscillatorHertz);
+                            Console.WriteLine("OscillatorHertz2: {0}", _ssrfStatus.OscillatorHertz2);
+                            Console.WriteLine("AirFlowHertz: {0}", _ssrfStatus.AirFlowHertz);
+                            Console.WriteLine("CurrentPowerWatts: {0}", _ssrfStatus.CurrentPowerWatts);
+                            Console.WriteLine("====================================================");
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Instrument not connected. Please connect instrument first.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in getting SSRF Measurements");
+                    Console.WriteLine(ex.Message);
+
+                }
+               
+            });
+            return _ssrfStatus;
+        }
+    }
+}
--- DataFetchService/Program.cs
+++ DataFetchService/Program.cs
@@ -1,33 +1,44 @@
-﻿using System;
+﻿using Agilent.AtomicSpectroscopy.ICP.Protocol.Scp;
+using Agilent.Nexus.Protocol.Scp;
+using Agilent.Nexus.ScpClient.Connection;
+using Agilent.Nexus.ScpClient.PublicInterfaces;
+using Agilent.Nexus.ScpClient.ScpHandler;
+using Agilent.Nexus.ScpClient.SLIP;
+using Agilent.Nexus.Xdr;
+using RFControlAsyncApp;
+using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace DataFetchService
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.SetOut(new ConsoleWriter());
             Console.WriteLine("Starting Data Fetch Service for P500...");
-            var ipAddress = "Invalid.IP.Address.Initialization";
-            
+            var ipAddress = "146.223.0.238";
+
             // Ask for IP address until user inputs valid IP address
             while (!Utils.isIPAddressValid(ipAddress))
             {
-                Console.Write("Instrument IP address: ");
-                ipAddress = Console.ReadLine();
+                Console.Write("Instrument IP address: {0}", ipAddress);
+                //ipAddress = Console.ReadLine();
+                Console.WriteLine();
             }
+            var dataFetchService = new DataFetchService();
+            dataFetchService.connect_To_Instrument(ipAddress);
 
-            Console.WriteLine("Connecting to {0}", ipAddress);
             for (int i = 0; i < 10; i++)
             {
-                Console.Write(".");
-                Thread.Sleep(1000);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Connected to {0}", ipAddress);
-            Console.ReadKey();
+                Task<AsyncSsrfStatus> _xyz = dataFetchService.getSSRFMeasurements();
+                // AsyncSsrfStatus _ssrfStatus = await _xyz.ConfigureAwait(fa);
 
+            }
+
+            Console.ReadKey();
         }
     }
 }
